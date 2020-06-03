fd8713e2937d9842118b2a8e21861252f65f7c14
MVuko123 <63670208+MVuko123@users.noreply.github.com>
2020-06-03 20:03:34 +0200

    Update Z01.java

--- Z01.java
+++ Z01.java
@@ -9,11 +9,11 @@ public class Z01 {
 		int br2 = Integer.parseInt(JOptionPane.showInputDialog("Unesite drugi broj"));
 		int br3 = Integer.parseInt(JOptionPane.showInputDialog("Unesite treci broj"));
 		
-		if(br1 > br2 && br1 > br3) {
+		if(br1 >= br2 && br1 >= br3) {
 			System.out.println(br1);
-		}else if(br2 > br1 && br2 > br3) {
+		}else if(br2 >= br1 && br2 >= br3) {
 			System.out.println(br2);
-		}else if(br3 > br1 && br3 > br2) {
+		}else if(br3 >= br1 && br3 >= br2) {
 		    System.out.println(br3);
 		}else {
 			System.out.println("Brojevi su jednaki");
